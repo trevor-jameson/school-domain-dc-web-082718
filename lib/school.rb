4596a20cb74f37276465abd4981c47bbe1a1aa0e
Trevor Jameson <github email address>
2018-08-29 20:17:04 -0400

    lesson learned

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+require 'pry'
+class School
+
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster[grade] # Does this key already exist?
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  # Have not been  able to pass #sort test yet
+  def sort
+    @roster.collect do |grade, students|
+      students.sort {|x, y| x[0] <=> y[0]}
+    end
+    # @roster.to_h
+  end
+end
